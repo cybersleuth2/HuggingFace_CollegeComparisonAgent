364d5bdabc0e0560dab44f950257458b322aa518
Sonu Kumar <sl4psh0t@users.noreply.huggingface.co>
2025-04-26 19:18:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,10 @@ final_answer = FinalAnswerTool()
 model = HfApiModel(
     max_tokens=2096,
     temperature=0.5,
-    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',  # Replace with actual model or API
+    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',  # it is possible that this model may be overloaded
+    custom_role_conversions=None,
+)
+
 )
 
 agent = CodeAgent(
